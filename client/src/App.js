5499faaceb101c3eabf75350ab8b3368fd33566b
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2023-06-08 20:39:52 -0300

    prueba

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>
+      ✅Acá comienza nuestra aventura y 
+      desafio como programadores, 
+      si vos podes, yo tambien puedo 💪
+      </h1>
+    </div>
+  );
+}
+
+export default App;
