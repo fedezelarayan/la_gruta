097a50fafdd173d45d6d24001648a4044c719957
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2023-06-09 11:21:16 -0300

    depenencias instaladas

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
-
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
      <h1>
       ✅Acá comienza nuestra aventura y 
       desafio como programadores, 
       si vos podes, yo tambien puedo 💪
       </h1>
+      </Routes>
     </div>
   );
 }
