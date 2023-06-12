485ecc7c3737d8871bce45c4ff477fb6cde1d217
sofiaparraweb <sofiaparraweb@gmail.com>
2023-06-12 10:30:22 -0300

    TiendaSofi

---
+++ client/src/components/Tienda/tienda.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tienda = () => {
+  return (
+    <div>
+      <h1>Hola, soy la Tienda</h1>
+      <p>Este es mi contenido</p>
+    </div>
+  );
+};
+
+export default Tienda;
