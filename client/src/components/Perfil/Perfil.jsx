603d86243a825d438623ef29a3b0810a2a36ab28
sofiaparraweb <sofiaparraweb@gmail.com>
2023-06-12 10:33:27 -0300

    PERFIL

---
+++ client/src/components/Perfil/Perfil.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Perfil = () => {
+  return (
+    <div>
+      <h1>Hola, soy el Perfil</h1>
+      <p>Este es mi contenido.</p>
+    </div>
+  );
+};
+
+export default Perfil;
