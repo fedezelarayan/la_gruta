d6c237f431d812b0d52a74b704603bb6787e70d0
jhoguero <jhoguero21@gmail.com>
2023-06-13 17:13:37 -0500

    complemento de rutas

--- api/src/routes/UserRouter.js
+++ api/src/routes/UserRouter.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+
 const UserRouter = Router();
 
 const {
---
+++ api/src/routes/mainRoutes.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+
+// import reouter
+
+/* const ActivityRouter = require("./ActivityRouter"); */
+const UserRouter = require("./UserRouter");
+/* const ProductsRouter = require("./ProductsRouter"); */
+/* const ChildrenRouter = require("./ChildrenRouter"); */
+/* const ProductsTypesRouter = require("./ProductsTypeRouter"); */
+/* const ActivityTypesRouter = require("./ActivityTypesRouter"); */
+const router = Router();
+
+
+
+// Rutes
+
+router.use("/user", UserRouter);
+/* router.use("/activity", ActivityRouter); */
+/* router.use("/products", ProductsRouter); */
+/* router.use("/children", ChildrenRouter); */
+/* router.use("/productsTypes", ProductsTypesRouter); */
+/* router.use("/activityTypes", ActivityTypesRouter); */
+
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+  });
+
+module.exports = router;
