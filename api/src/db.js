bb011a6f7f9bef014389cfefd17afec4ce57241c
Gian <gianpolastri@gmial.com>
2023-06-14 11:29:35 -0300

    Ejecución de la función uploader al levantarse DB y server en el index

--- api/src/db.js
+++ api/src/db.js
@@ -5,8 +5,8 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
 const sequelize = new Sequelize(
-    // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-    DB_DEPLOY,
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+    // DB_DEPLOY,
     { logging: false, native: false }
 );
 
