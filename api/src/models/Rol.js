85a66b970441655117e46be2917978e61b00ac95
Gian <gianpolastri@gmial.com>
2023-06-15 20:00:51 -0300

    Creación del modelo de Rol

---
+++ api/src/models/Rol.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+
+    sequelize.define('Rol', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        timestamps: false,
+      }
+    )
+}
