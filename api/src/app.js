43fef3d21c466c31891902a6b0406070f761de2b
sofiaparraweb <sofiaparraweb@gmail.com>
2023-06-27 02:31:22 -0300

    nodemailer

--- api/src/app.js
+++ api/src/app.js
@@ -15,11 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-<<<<<<< HEAD
-  res.header('Access-Control-Allow-Origin', "*"); // http://localhost:3000  update to match the domain you will make the request from
-=======
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
->>>>>>> bfc59ce (cambios user)
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
