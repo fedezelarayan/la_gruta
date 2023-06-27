0219e3a14659d430e5aaf6bc84d365634ff126da
Gian <gianpolastri@gmial.com>
2023-06-27 16:51:19 -0300

    Modelo de donaciones agregado

--- api/src/db.js
+++ api/src/db.js
@@ -39,7 +39,7 @@ let capsEntries = entries.map((entry) => [
 
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { Activity, ActivityType, Children, Products, ProductsType, User, Rol, Cart, Cart_Products } = sequelize.models;
+const { Activity, ActivityType, Children, Products, ProductsType, User, Rol, Cart, Cart_Products, Donation } = sequelize.models;
 
 Activity.belongsToMany(ActivityType, { through: "Activity_ActivityType" });
 ActivityType.belongsToMany(Activity, { through: "Activity_ActivityType" });
@@ -61,6 +61,9 @@ Cart.belongsTo(User);
 
 Products.belongsToMany(Cart, { through: Cart_Products});
 Cart.belongsToMany(Products, { through: Cart_Products});
+
+User.hasMany(Donation);
+Donation.belongsTo(User);
 
 module.exports = {
     ...sequelize.models,
