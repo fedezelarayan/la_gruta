774a8361467871bd418a5535b1db0c174da357fe
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2023-06-29 10:19:58 -0300

    prueba

--- api/src/app.js
+++ api/src/app.js
@@ -3,10 +3,12 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/mainRoutes.js');
+const cors = require('cors');
 
 require('./db.js');
 
 const server = express();
+
 
 server.name = 'API';
 
@@ -15,12 +17,21 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*"); // http://localhost:3000  update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', "http://localhost:5173/"); // http://localhost:3000  update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+//=====>>>cors agregado<<<=====//
+server.use(
+  cors({
+  origin: "*",
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    maxAge: 86400,
+  })
+);
 
 server.use('/', routes);
 
