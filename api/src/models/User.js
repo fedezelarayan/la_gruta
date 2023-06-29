b7c48af3c71615d8395035d099daf92b0e3ab9f5
jhoguero <jhoguero21@gmail.com>
2023-06-29 09:42:26 -0500

    ultimo commit con datos de sofi

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -13,64 +13,62 @@ module.exports = (sequelize) => {
             allowNull: false,
         },
         username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: true,
+        },
+        birthDate: {
+            type: DataTypes.STRING,
+            // defaultValue: 0,
+        },
+        image: {
+            type: DataTypes.STRING,
+            defaultValue: "",
+            isUrl: {
+                msg: "La imagen debe ser una URL"
+            }
+        },
+        phone: {
+            type: DataTypes.STRING,
+            defaultValue: 0,
+        },
+        mail: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            isEmail: {
+                msg: "Debe ingresar un email válido"
+            }
+        },
+        occupation: {
+            type: DataTypes.STRING,
 
-            username: {
-                type: DataTypes.STRING,
-                unique: true,
-                allowNull: true,
-            },
-            birthDate: {
-                type: DataTypes.STRING,
-                // defaultValue: 0,
-            },
-            image: {
-                type: DataTypes.STRING,
-                defaultValue: "",
-                isUrl: {
-                    msg: "La imagen debe ser una URL"
-                }
-            },
-            phone: {
-                type: DataTypes.STRING,
-                defaultValue: 0,
-            },
-            mail: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                unique: true,
-                isEmail: {
-                    msg: "Debe ingresar un email válido"
-                }
-            },
-            occupation: {
-                type: DataTypes.STRING,
-
-            },
-            address: {
-                type: DataTypes.STRING,
-                allowNull: true,
-            },
-            password: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                defaultValue: 12345678,
-                isAlphanumeric: {
-                    msg: "La contraseña debe ser alfanumérica"
-                }, //Chequea que sea alfanumerico
-                // len: {
-                //     arg: [[6, 14]],
-                //     msg: "La contraseña debe tener entre 6 y 14 caracteres"
-                // }, //longitud de la contraseña
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 12345678,
+            isAlphanumeric: {
+                msg: "La contraseña debe ser alfanumérica"
+            }, //Chequea que sea alfanumerico
+            // len: {
+            //     arg: [[6, 14]],
+            //     msg: "La contraseña debe tener entre 6 y 14 caracteres"
+            // }, //longitud de la contraseña
 
 
-            },
-            status: {
-                type: DataTypes.BOOLEAN,
-                defaultValue: true
-            },
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
         },
     },
-    {
-        paranoid: true,
-    }
-)}
+{
+    paranoid: true,
+}
+    )
+}
