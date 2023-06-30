147f0d5005dc3304672f5a4fc495c1260e8e3dab
Gian <gianpolastri@gmial.com>
2023-06-30 18:26:59 -0300

    Donation webhook y ngrok

--- api/src/routes/paymentRouter.js
+++ api/src/routes/paymentRouter.js
@@ -1,15 +1,18 @@
 const { Router } = require('express');
-const { cartOrderHandler, donationOrderHandler, successCartHandler, webhookCartHandler } = require("../handlers/paymentHandler");
+const { cartOrderHandler, donationOrderHandler, successCartHandler, webhookCartHandler, webhookDontaionHandler } = require("../handlers/paymentHandler");
 
 const paymentRouter = Router();
 
 paymentRouter.post("/cart/create-order/:user_id", cartOrderHandler);
 
+paymentRouter.get("/cart/webhook", webhookCartHandler);
+
 paymentRouter.post("/donation/create-order/", donationOrderHandler);
+
+paymentRouter.post("/donation/webhook", webhookDontaionHandler)
 
 // peymentRouter.get("/cart/success", successCartHandler);
 
-peymentRouter.get("/cart/webhook", webhookCartHandler);
 
 
 module.exports = paymentRouter
